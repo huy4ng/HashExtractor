6008560a926efab06b47b4326e727d539ca8a2ca
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-12 17:10:04 +0200

    Update Generate-Exec.ps1

--- Generate-Exec.ps1
+++ Generate-Exec.ps1
@@ -1,6 +1,6 @@
 # Write The IP After the Script in the Command Line
 $template = Get-Content '.\DoTheThing-Template.ps1' | Out-String
 Remove-Item '.\DoTheThing.ps1'
-('$ip = ' + "'" + $args[0] + "'" + $template) | Out-file '.\DoTheThing.ps1'
+('$ip = ' + "'" + $args[0] + "'" + $template) | Out-file '.\DoTheThing.ps1' utf8
 start-process git-bash
 Write-Host "git add 'DoTheThing.ps1';git commit -m 'Create DoTheThing.ps1';git push"

